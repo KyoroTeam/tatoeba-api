3267ececa4c3c2b539f346c5b237efe36ec81674
James <james.waugh.r@gmail.com>
2021-02-24 22:23:31 -0500

    Add Lambda

--- lib/cdk2-stack.ts
+++ lib/cdk2-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from "path";
 
 export class Cdk2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const fn = new lambda.Function(this, "MyFunction", {
+      runtime: lambda.Runtime.DOTNET_CORE_3_1,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler")),
+    });
   }
 }
